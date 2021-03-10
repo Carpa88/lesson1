9ed41f3d4b0ab6508211c075eab112bffff9a054
Carpa88 <olenka_311@mail.ru>
2021-03-10 15:51:18 +0300

    Install webpack, webpack-server, bebel, style-loader, css-loader

---
+++ build/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+alert('Hello, World!');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9sZXNzb24xLy4vc3JjL2luZGV4LmpzIl0sIm5hbWVzIjpbImFsZXJ0Il0sIm1hcHBpbmdzIjoiOzs7OztBQUFBQSxLQUFLLENBQUUsZUFBRixDQUFMLEMiLCJmaWxlIjoiYnVuZGxlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiYWxlcnQgKCdIZWxsbywgV29ybGQhJyk7XHJcblxyXG5cclxuIl0sInNvdXJjZVJvb3QiOiIifQ==
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './index.html'
+  })],
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'build'),
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      }
+    ]
+  }
+};
