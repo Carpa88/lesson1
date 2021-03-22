3995ec390fb39e6323c815366078444c00af5ad3
Carpa88 <olenka_311@mail.ru>
2021-03-22 09:10:13 +0300

    add item in list

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
+
+let div = document.createElement('div');
+let p = document.createElement('p');
+let button = document.createElement('button');
+div.className = "text_list-box";
+
+p.innerHTML = "Всем привет! Вы прочитали важное сообщение.";
+p.className = "text_list_item";
+
+button.innerText = 'Delete';
+button.className = "text_list-button";
+
+document.body.append(div);
+div.append(p, button);
