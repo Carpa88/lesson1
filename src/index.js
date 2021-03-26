bdac834a11c8afd339333130a0c6dca259de9dfe
Carpa88 <olenka_311@mail.ru>
2021-03-26 11:28:12 +0300

    add line-through for checkbox

--- src/index.js
+++ src/index.js
@@ -8,18 +8,28 @@ function addItem () {
   let div1 = document.createElement('div');
   let div2 = document.createElement('div');
   let p = document.createElement('p');
+  let label = document.createElement('label');
+  let input = document.createElement('input');
   let a = document.createElement('a');
 
-  div1.className = "text_list_box card";
-  div2.className = "card-body";
-
-  input.value = 'dsfbsdr;jk;sejkrbnsk;rjbn';
+  
+  input.value = 'SLint крайне необходим при работе в команде. Зачем? Да опять же, все очень просто! Кто-то привык ставить в конце точку с запятой, кто-то нет, кто-то максимум пишет 80 символов в строку, кто-то 120, а кто-то громоздит вообще сколько душе угодно. В конечно итоге - ваш код или код вашей команды может превратиться в сплошную кашу.';
   
   if (input.value) {
-  p.innerHTML = input.value;
+  div1.className = "card";
+  div2.className = "card-body";
   p.className = "card-text";
+
+  input.type = 'checkbox';
+  input.className = 'form-check-input';
+  input.id="flexCheckDefault";
+  input.onchange = lineThrough;
+
+  label.innerHTML = input.value;
+  label.className = 'form-check-label';
+  label.for = 'flexCheckDefault';
   
-  a.className = "button_delete btn btn-primary";  
+  a.className = "btn btn-primary";  
   a.innerHTML = "Delete";
   a.href = '#';
   a.onclick = deleteItem;
@@ -27,6 +37,7 @@ function addItem () {
   document.getElementById('container').append(div1);
   div1.append(div2);
   div2.append(p, a);
+  p.append(input, label);
 
   input.value = '';
   
@@ -49,6 +60,14 @@ function deleteError(){
 }
     
 function deleteItem(){
- let divDel = this.closest('.text_list_box');
+ let divDel = this.closest('.card');
  divDel.remove();
+};
+
+function lineThrough(){
+  if( this.checked ) {
+    this.nextElementSibling.classList.add("line-through");
+  }else{
+    this.nextElementSibling.classList.remove("line-through");
+  };
 };
