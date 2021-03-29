b61b7eaa58354b5535a56f33c2d6e0bab988e11a
Carpa88 <olenka_311@mail.ru>
2021-03-29 14:57:38 +0300

    replaced checkbox

--- src/index.js
+++ src/index.js
@@ -9,32 +9,29 @@ function addItem () {
   let div2 = document.createElement('div');
   let p = document.createElement('p');
   let label = document.createElement('label');
-  let checkbox = document.createElement('input');
   let a = document.createElement('a');
+  
+  input.value = 'Get started with Bootstrap via npm with our starter project! Head to the twbs/bootstrap-npm-starter template repository to see how to build and customize Bootstrap in your own npm project. Includes Sass compiler, Autoprefixer, Stylelint, PurgeCSS, and Bootstrap Icons.'
 
   if (input.value) {
   div1.className = "card";
   div2.className = "card-body";
   p.className = "card-text";
 
-  checkbox.type = 'checkbox';
-  checkbox.className = 'form-check-input';
-  checkbox.id="flexCheckDefault";
-  checkbox.onchange = lineThrough;
-
   label.innerHTML = input.value;
   label.className = 'form-check-label';
-  label.for = 'flexCheckDefault';
   
   a.className = "btn btn-primary";  
   a.innerHTML = "Delete";
   a.href = '#';
   a.onclick = deleteItem;
   
-  document.getElementById('container').append(div1);
-  div1.append(div2);
-  div2.append(p, a);
-  p.append(checkbox, label);
+  document.getElementById('container').appendChild(div1);
+  div1.appendChild(div2);
+  div2.appendChild(p, a);
+  p.appendChild(label);
+  label.insertAdjacentHTML('afterbegin', '<input type="checkbox" class="form-check-input" onchange="lineThrough()"></input>');
+  
 
   input.value = '';
   
